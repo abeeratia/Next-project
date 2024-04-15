0a399fcb0cddeebaf1866f23ba9eda4eaf9cf00f
abeeratia <abeeryousef198@gmail.com>
2024-04-15 15:48:51 +0200

    not found

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div style={{backgroundColor:'red', height:"100vh"}} className='vh-100 bg-danger'>
+
+     <h1  className='text-danger'>404544444444</h1>
+    </div>
+  )
+}
+
+export default NotFound
